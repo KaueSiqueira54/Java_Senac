73877a332a3d511bcc006bfa6b36aad8a9470ef9
KaueSiqueira <kaue54150@gmail.com>
2026-07-28 14:18:04 -0300

    Atividade calculadora

---
+++ Aula_02/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+package calculadora;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    static public void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        Numero n1 = new Numero();
+        Numero n2 = new Numero();
+        Numero res = new Numero();
+
+        int opc = 1;
+
+        while(opc == 1) {
+            System.out.println("Digite o valor 1: ".formatted(n1));
+            n1.setValor(leitura.nextInt());
+
+            System.out.println("Digite o valor 2: ".formatted(n1));
+            n2.setValor(leitura.nextInt());
+
+            res.setValor(n1.getValor() + n2.getValor());
+            System.out.println("A soma de %d + %d = %d: ".formatted(n1.getValor(), n2.getValor(), res.getValor()));
+
+            System.out.println("""
+                    Deseja continuar?
+                    1 - Sim
+                    2- Não
+                    """);
+            opc = leitura.nextInt();
+            leitura.nextLine();
+        }
+        System.out.println("Obrigado por usar o sistema!");
+    }
+}
